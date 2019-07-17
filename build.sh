0954a79deaf29d79790e840b5adbad4bc5c8909e
Harlen Batagelo <hbatagelo@gmail.com>
2019-07-16 22:39:07 -0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# Reset build
+mkdir -p build && cd build
+
+# Configure
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+# Build
+cmake --build . --config Debug -- -j $(nproc)
+
+# Test
+ctest -j $(nproc) --output-on-failure
+
