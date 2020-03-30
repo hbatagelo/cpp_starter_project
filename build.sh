7096272e43a17564bb9791484e17fd6deaaa3eeb
Harlen Batagelo <harlen.batagelo@gmail.com>
2020-03-30 20:23:30 -0300

    Fix lcov

--- build.sh
+++ build.sh
@@ -18,19 +18,19 @@ ctest -j $(nproc) --output-on-failure -VV
 
 # Coverage
 #if [ -n TRAVIS ]; then
-   COV=$(cmake -LA -N . | grep ENABLE_COVERAGE:BOOL=ON)
-   if [[ ! -z "$COV" && -n TRAVIS ]]; then # Does CMake's cache has ENABLE_COVERAGE:BOOL=ON?
-       # Generate coverage report
-       if command -v lcov >/dev/null 2>&1; then # Does lcov exist?
-           printf '\n'
-           echo -e "\e[93m-- Generating lcov info\e[39m"
-           lcov --capture --directory . --output-file coverage.info
-           echo -e "\e[93m-- Filtering out dependencies\e[39m"
-           lcov --remove coverage.info '*conan/data/*' '/usr/*' --output-file coverage.info
-           echo -e "\e[93m-- Generating HTML report\e[39m"
-           genhtml coverage.info --output-directory coverage_report
-           echo -e "\e[93m-- Generating done\e[39m"
-       fi
-   fi
+#    COV=$(cmake -LA -N . | grep ENABLE_COVERAGE:BOOL=ON)
+#    if [[ ! -z "$COV" && -n CI ]]; then # Does CMake's cache has ENABLE_COVERAGE:BOOL=ON?
+#        # Generate coverage report
+#        if command -v lcov >/dev/null 2>&1; then # Does lcov exist?
+#            printf '\n'
+#            echo -e "\e[93m-- Generating lcov info\e[39m"
+#            lcov --capture --directory . --output-file coverage.info
+#            echo -e "\e[93m-- Filtering out dependencies\e[39m"
+#            lcov --remove coverage.info '*conan/data/*' '/usr/*' --output-file coverage.info
+#            echo -e "\e[93m-- Generating HTML report\e[39m"
+#            genhtml coverage.info --output-directory coverage_report
+#            echo -e "\e[93m-- Generating done\e[39m"
+#        fi
+#    fi
 #fi
 #--base-directory .
