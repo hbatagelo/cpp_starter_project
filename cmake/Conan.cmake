f247d522e7cdc88acc2cb22c46f46ae4b8ce6583
Harlen Batagelo <harlen.batagelo@gmail.com>
2020-07-19 20:44:19 -0300

    Revert fmt and spdlog version requirements

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -24,8 +24,8 @@ macro(run_conan)
     ${CONAN_EXTRA_REQUIRES}
     catch2/[>=2.11.0]
     docopt.cpp/[>=0.6.2]
-    fmt/6.1.2
-    spdlog/1.6.0
+    fmt/[>=6.1.2]
+    spdlog/[>=1.6.0]
     OPTIONS
     ${CONAN_EXTRA_OPTIONS}
     BASIC_SETUP
